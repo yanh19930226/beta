865bf2e5220c5e39b331a450dbb4921537c03ec0
yanh1993 <891367701@qq.com>
2020-01-01 14:37:22 +0800

    up

---
+++ User.API/Resilience.EventBus/Abstractions/IIntegrationEventHandler.cs
@@ -0,0 +1,18 @@
+﻿using Resilience.EventBus.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resilience.EventBus.Abstractions
+{
+    public interface IIntegrationEventHandler<in TIntegrationEvent> : IIntegrationEventHandler
+        where TIntegrationEvent : IntegrationEvent
+    {
+        Task Handle(TIntegrationEvent @event);
+    }
+
+    public interface IIntegrationEventHandler
+    {
+    }
+}
---
+++ User.API/Resilience.EventBusServiceBus/IServiceBusPersisterConnection.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Resilience.EventBusServiceBus
+{
+    public interface IServiceBusPersisterConnection : IDisposable
+    {
+        ServiceBusConnectionStringBuilder ServiceBusConnectionStringBuilder { get; }
+
+        ITopicClient CreateModel();
+    }
+}
