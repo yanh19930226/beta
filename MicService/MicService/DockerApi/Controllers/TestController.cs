511702d32a2874f0687779bbfbfa0199ed6f0d03
yanh1993 <891367701@qq.com>
2020-05-16 18:28:25 +0800

    'up'

---
+++ MicService/MicService/DockerApi/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DockerApi.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DockerApi.Controllers
+{
+    [Route("api/test")]
+    [ApiController]
+    public class TestController : Controller
+    {
+        private ConDbContext _conContext;
+
+        public TestController(ConDbContext conContext)
+        {
+            _conContext = conContext;
+        }
+        public IActionResult Index()
+        {
+            var res = _conContext.Users.ToList();
+            return Ok(res);
+        }
+    }
+}
---
+++ MicService/MicService/DockerApi/Data/ConDbContext.cs
@@ -0,0 +1,26 @@
+﻿using DockerApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DockerApi.Data
+{
+    public class ConDbContext:DbContext
+    {
+        public ConDbContext(DbContextOptions<ConDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<User>()
+                .ToTable("Users")
+                .HasKey(u => u.Id);
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
