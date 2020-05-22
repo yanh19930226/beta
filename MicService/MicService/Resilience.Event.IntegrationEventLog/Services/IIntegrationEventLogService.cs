495b727224b258f9c3c9ffa47d8b99268cefe7d3
yanh1993 <891367701@qq.com>
2020-05-23 02:06:39 +0800

    'up'

---
+++ MicService/MicService/Resilience.Event.IntegrationEventLog/Services/IIntegrationEventLogService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Storage;
+using Resillience.EventBus.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resilience.Event.IntegrationEventLog.Services
+{
+    public interface IIntegrationEventLogService
+    {
+        Task<IEnumerable<IntegrationEventLogEntry>> RetrieveEventLogsPendingToPublishAsync(Guid transactionId);
+        Task SaveEventAsync(IntegrationEvent @event, IDbContextTransaction transaction);
+        Task MarkEventAsPublishedAsync(Guid eventId);
+        Task MarkEventAsInProgressAsync(Guid eventId);
+        Task MarkEventAsFailedAsync(Guid eventId);
+    }
+}
