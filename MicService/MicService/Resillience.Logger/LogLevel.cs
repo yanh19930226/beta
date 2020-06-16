61013c9964982628e28b7a8f0235b40d54c863ac
yanh <891367701@qq.com>
2020-06-16 14:36:03 +0800

    'up'

---
+++ MicService/MicService/Resillience.Logger/LogLevel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Resillience.Logger
+{
+    public enum LogLevel
+    {
+        Verbose,
+        Debug,
+        Information,
+        Warning,
+        Error,
+        Fatal
+    }
+}
